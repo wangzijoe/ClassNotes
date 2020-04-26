bc82f120369835507ef7f0c24364a8736906a314
wangzijoe <wangzijoe@qq.com>
2020-04-26 23:36:37 +0800

    File

--- src/main/java/com/example/utils/FileUtils.java
+++ src/main/java/com/example/utils/FileUtils.java
@@ -1,8 +1,17 @@
 package com.example.utils;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.LinkedList;
 import java.util.List;
+
+import org.springframework.util.StringUtils;
 
 public class FileUtils {
 
@@ -20,6 +29,50 @@ public class FileUtils {
 			}
 		}
 		return fileTree;
+	}
+
+	public static void readFileByIO(String filePath) {
+		if (StringUtils.isEmpty(filePath)) {
+			return;
+		}
+		FileInputStream inputStream = null;
+		BufferedReader reader = null;
+		try {
+			inputStream = new FileInputStream(filePath);
+			reader = new BufferedReader(new InputStreamReader(inputStream));
+			String str = null;
+			while ((str = reader.readLine()) != null) {
+				// 输出文件内容
+				System.err.println(str);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (null != inputStream) {
+				try {
+					inputStream.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	public static void readFileByChannel(String filePath) {
+		if (StringUtils.isEmpty(filePath)) {
+			return;
+		}
+		try {
+			List<String> lists = Files.readAllLines(Paths.get(filePath));
+			for (String str : lists) {
+				// 输出文件内容
+				System.err.println(str);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
---
+++ src/test/java/com/example/file/FileTest.java
@@ -0,0 +1,27 @@
+package com.example.file;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.Test;
+
+import com.example.utils.FileUtils;
+
+
+public class FileTest {
+
+	
+	@Test
+	public void testReadFile() {
+		FileUtils.readFileByChannel("D:\\workSapces\\Japanese\\中级_上\\No_06.md");
+	}
+	
+	@Test
+	public void testReadFile1() throws Exception {
+		Path path = Paths.get("D:\\workSapces\\Japanese\\中级_上\\No_06.md");
+		String readString = Files.readString(path);
+		System.err.println(readString);
+	}
+}
